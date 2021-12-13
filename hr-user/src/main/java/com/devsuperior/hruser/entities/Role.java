b19287baf08e3e91fb340f4037ba76556d58b5d1
GabrielRodrigues07 <grodrigues256@gmail.com>
2021-12-13 16:56:50 -0300

    db config server

--- hr-user/src/main/java/com/devsuperior/hruser/entities/Role.java
+++ hr-user/src/main/java/com/devsuperior/hruser/entities/Role.java
@@ -1,15 +1,12 @@
 package com.devsuperior.hruser.entities;
 
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 @Entity
-@Table(name = "tb_role")
+@Table(name= "tb_role")
 public class Role implements Serializable {
     private static final long serialVersionUID = 1L;
 
