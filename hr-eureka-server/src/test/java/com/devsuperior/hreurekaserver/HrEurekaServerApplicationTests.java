ccd10da2250b8ce402550ab1ab177bbc4654afd4
GabrielRodrigues07 <grodrigues256@gmail.com>
2021-12-19 15:36:57 -0300

    hr-eureka-server container

--- hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -1,13 +1,10 @@
 package com.devsuperior.hreurekaserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HrEurekaServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class HrEurekaServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
