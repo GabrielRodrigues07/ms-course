edc432545bfc600df4d5984f1c2fd33c4780b2a7
GabrielRodrigues07 <grodrigues256@gmail.com>
2021-12-19 16:43:09 -0300

    update imports

--- hr-user/src/main/java/com/devsuperior/hruser/repositories/UserRepository.java
+++ hr-user/src/main/java/com/devsuperior/hruser/repositories/UserRepository.java
@@ -1,8 +1,7 @@
 package com.devsuperior.hruser.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsuperior.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
